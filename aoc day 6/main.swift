948d39835dac0332066249a108bb15337b8aa207
Moritz Diedenhofen <moritzdiedenhofen@googlemail.com>
2022-01-02 19:22:14 +0100

    cleaned

--- aoc day 6/main.swift
+++ aoc day 6/main.swift
@@ -7,15 +7,15 @@ func loadData() -> String{
         return ""
     }
 }
+
 var fishAges = loadData().split(separator: ",").map {Int($0)!}
 
-print("0: \(fishAges)")
-
-for day in 1 ... 256  {
+for day in 1 ... 80  {
     
     let newBabieCount = fishAges.filter {$0 == 0}.count
     fishAges.append(contentsOf: Array(repeating: 9, count: newBabieCount))
     fishAges = fishAges.map {$0 == 0 ? $0 + 6 : $0 - 1}
+    
 }
 
 print("total fishes: \(fishAges.count)")
