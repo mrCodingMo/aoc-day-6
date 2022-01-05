45c0fc7cad0400cb7629878c588968131fdc780e
Moritz Diedenhofen <moritzdiedenhofen@googlemail.com>
2022-01-05 17:10:02 +0100

    solution part 2

--- aoc day 6/main.swift
+++ aoc day 6/main.swift
@@ -9,13 +9,29 @@ func loadData() -> String{
 }
 
 var fishAges = loadData().split(separator: ",").map {Int($0)!}
+var fishes = Array(repeating: 0, count: 10)
 
-for day in 1 ... 80  {
-    
-    let newBabieCount = fishAges.filter {$0 == 0}.count
-    fishAges.append(contentsOf: Array(repeating: 9, count: newBabieCount))
-    fishAges = fishAges.map {$0 == 0 ? $0 + 6 : $0 - 1}
-    
+for i in 0 ... 8 {
+    fishes[i] = fishAges.filter {$0 == i}.count
 }
 
-print("total fishes: \(fishAges.count)")
+for day in 1 ... 256  {
+    
+    //Create babies
+    fishes[9] += fishes[0]
+    
+    //Reset age
+    fishes[7] += fishes[0]
+    fishes[0] = 0
+    
+    var newFishes = Array(repeating: 0, count: 10)
+    
+    for i in stride(from: 9, to: 0, by: -1) {
+        newFishes[i - 1] = fishes[i]
+    }
+    
+    fishes = newFishes
+}
+
+print("total: \(fishes.reduce(0, +))")
+
